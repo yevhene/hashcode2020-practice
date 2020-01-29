22dc1dbdbd66802b7a10594c10bccf070b5e0c22
Paul Druziak <paul.druziak@icloud.com>
2020-01-29 22:41:18 +0200

    first verson

---
+++ dynamic.sh
@@ -0,0 +1,5 @@
+ruby dynamic.rb < in/a_example.in > out/a_example.out
+ruby dynamic.rb < in/b_small.in > out/b_small.out
+ruby dynamic.rb < in/c_medium.in > out/c_medium.out
+ruby dynamic.rb < in/d_quite_big.in > out/d_quite_big.out
+ruby dynamic.rb < in/e_also_big.in > out/e_also_big.out
